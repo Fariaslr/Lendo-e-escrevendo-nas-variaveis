75e85a352b5f49ffe3ce74b4900f4ba4312bcacc
Lucas Farias <83430368+Fariaslr@users.noreply.github.com>
2022-12-03 11:28:22 -0300

    :hammer:

--- 9 - Reais em ordem inversa.c
+++ 9 - Reais em ordem inversa.c
@@ -2,7 +2,7 @@
 #include <locale.h>//biblioteca de acentos
 
 /*
-	9) Faça um programa que leia dois valores do tipo float. Use um único comando de 
+	9) FaÃ§a um programa que leia dois valores do tipo float. Use um Ãºnico comando de 
 	leitura para isso. Em seguida, imprima os valores lidos na ordem inversa em que eles 
 	foram lidos.  
 */
@@ -10,12 +10,12 @@
 int main(){
 	
     setlocale(LC_ALL,"");//Permite caracteres especiais
-    float n1,n2;
+    float primeiroNumero,segundoNumero;
 
     printf("Escreva dois valores: ");
-    scanf("%f %f",&n1,&n2);
+    scanf("%f %f",&primeiroNumero,&segundoNumero);
 
-    printf("Os numeros estão em ordem trocada %.1f e %.1f\n",n2,n1);
+    printf("Os numeros estÃ£o em ordem trocada %.1f e %.1f\n",segundoNumero,primeiroNumero);
     
     return 0;
 }
